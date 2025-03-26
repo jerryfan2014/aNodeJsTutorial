792a7718b071ab07662ae59e0f401403e0df4245
jerryfan <jerryfan@kdvtc.edu.cn>
2025-03-26 10:49:16 +0800

    ch4 webServer开发

--- ch3文件系统/goods/deleteGoods.js
+++ ch3文件系统/goods/deleteGoods.js
@@ -1,37 +1,27 @@
 // 变现对文件做CRUD思路最重要
-//todo 修改goods.json文件中id为2的商品信息
-// {"id":2,"name":"PC008-1 with diode","price":216} -> {"id":2,"name":"KKKKKKKDDDDDDDD","price":7777}
 const fs = require("fs")
+
 fs.readFile("./ch3文件系统/goods/goods.json","utf-8", function(err, data){
     if (err) throw err;
     // 把文件内容转换成（数组）对象
     // 增加一个文件非空判断，如果文件为空，不需要 JSON.parse(data)；直接构造对象
     let goods = JSON.parse(data);
-    let param = {         
-        "name": "TTTT", 
-        "price": 6666}
-    // 遍历找到id=2的那个商品
+
+    // 删除id=2的商品
     for (i = 0; i < goods.length; i++) {
         // 找到id=2的商品
         if (goods[i].id == 2) {
-            // 非通用实现
-            // goods[i].name = 'KKKKKKKDDDDDDDD';
-            // goods[i].price = '7777';
-            // 通用实现，遍历param
-            for (k in param) {
-                if (goods[i][k]) {
-                    goods[i][k] = param[k];
-                }
-            }
-            // 优化，不用对后续元素空循环
-            // break; 
+            // 删除指定元素
+           goods.splice(i, 1) 
         }
-    }        
+    }  
+
     // 把goods对象转换成字符串
     let goodStr = JSON.stringify(goods);
     // 回写更新文件
     fs.writeFile("./ch3文件系统/goods/goods.json", goodStr, function(err){
         if (err) throw err;
+
         console.log("文件写入成功");
     })
 })
---
+++ ch4webServer开发/fileAcc/server_static.js
@@ -0,0 +1,80 @@
+var http = require('http'); 
+var fs = require('fs'); // 用于读取静态资源
+var path = require('path'); // 用于做路径拼接
+var urlModule = require('url'); 
+var server = http.createServer(); 
+// 定义不同文件对应的文件类型
+var mimeTypes = { 
+ "css": "text/css;charset=utf-8", 
+ "jpg": "image/jpeg", 
+ "html": "text/html;charset=utf-8" ,
+ }; 
+server.on('request', function(request, response) { 
+	 //获取静态资源路径
+	var url = urlModule.parse(request.url).pathname; 
+	 if (url === '/') { 
+	 //读取相应静态资源内容
+		 fs.readFile(path.join(__dirname, 'static/index.html'), 'utf-8', function(err, data) { 
+		 //如果出现异常，则抛出异常
+			 if (err) { 
+				console.log(err); 
+			 }else{ 
+				 //将读取的静态资源文件响应给浏览器
+				 response.setHeader('content-type', mimeTypes['html']); 
+				 response.end(data);
+			} 				 
+		 }); 
+	 } else if (url === '/product.html') { 
+		 fs.readFile(path.join(__dirname, 'static/product.html'), 'utf-8',function(err, data) { 
+			 if (err) { 
+				console.log(err); 
+			 }else{ 
+				 response.setHeader('content-type', mimeTypes['html']); 
+				 response.end(data);
+			 } 
+		 }); 
+	 //如果有图片、CSS 文件等，浏览器会重新发送请求获取静态资源
+	 } else if (url === '/css/main.css') { 
+		 var cssPath = path.join(__dirname, 'static/css/main.css') 
+		 fs.readFile(cssPath, 'utf-8', function(err, data) { 
+			 if (err) { 
+				console.log(err); 
+			 }else{ 
+				 response.setHeader('content-type', mimeTypes['css']); 
+				 response.end(data);
+			} 
+		 }); 
+	 } else if (url === '/images/pro-1.jpg') { 
+		 var imgPath = path.join(__dirname,'static/images/pro-1.jpg') 
+		 fs.readFile(imgPath, function(err, data) { 
+			 if (err) { 
+				console.log(err); 
+			 }else{ 
+				 response.setHeader('content-type', mimeTypes['jpg']); 
+				 response.end(data);
+			} 
+		 }); 
+	 } else if (url === '/images/pro-2.jpg') { 
+		 var imgPath = path.join(__dirname,'static/images/pro-2.jpg') 
+		 fs.readFile(imgPath, function(err, data) { 
+			 if (err) { 
+				console.log(err); 
+			 }else{ 
+				 response.setHeader('content-type', mimeTypes['jpg']); 
+				 response.end(data);
+			} 
+		 }); 
+	 } else { 
+		 fs.readFile(path.join(__dirname, 'static/404.html'), 'utf-8', function(err, data) { 
+			 if (err) { 
+				console.log(err); 
+			 }else{ 
+				 response.setHeader('content-type', mimeTypes['html']); 
+				 response.end(data);
+			} 
+		 }); 
+	 } 
+}); 
+server.listen(3000, function() { 
+ console.log('listening port 3000...'); 
+});
